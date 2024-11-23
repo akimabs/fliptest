4d0108e4ee3488e9727c0678c5471abaccb38250
akimabs <kimuwhehe@gmail.com>
2024-11-23 21:35:42 +0700

    docs: update comment env

--- src/api/request/transaction.ts
+++ src/api/request/transaction.ts
@@ -2,13 +2,17 @@ import {get, withQuery} from '@api/index';
 import PostAPISchema from '@api/schema/transaction.schema';
 import {TransactionAPIType} from '@api/type/transaction.type';
 
-// for this moment, think this use dotenv(for react native) and improve the native side for better security
-const BASE_URL = ' https://recruitment-test.flip.id/';
+/**
+ * For now, assume this uses dotenv (for React Native)
+ * but for production, consider improving the native side for better security.
+ */
+
+const BASE_URL = 'https://recruitment-test.flip.id';
 
 export const getTransaction = withQuery(
   '/frontend-test',
   (params: TransactionAPIType.Transaction.Request) =>
-    get<TransactionAPIType.Transaction.Response>('/frontend-test ', {
+    get<TransactionAPIType.Transaction.Response>('/frontend-test', {
       baseURL: BASE_URL,
       params,
     }),
